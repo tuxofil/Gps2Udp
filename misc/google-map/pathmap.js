8dc2673a577f9a2393f9ad3401f4f313da6dcd3f
Aleksey Morarash <aleksey.morarash@gmail.com>
2014-09-11 11:28:17 +0300

    replace tabs with spaces

--- misc/google-map/pathmap.js
+++ misc/google-map/pathmap.js
@@ -18,16 +18,16 @@ var globalPolyline;
 function getXmlHttp(){
     var xmlhttp;
     try {
-	xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
+        xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
     } catch (e) {
-	try {
-	    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	} catch (E) {
-	    xmlhttp = false;
-	}
+        try {
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        } catch (E) {
+            xmlhttp = false;
+        }
     }
     if (!xmlhttp && typeof XMLHttpRequest != 'undefined') {
-	xmlhttp = new XMLHttpRequest();
+        xmlhttp = new XMLHttpRequest();
     }
     return xmlhttp;
 }
@@ -36,14 +36,14 @@ function requestPointsLoad(){
     var xmlhttp = getXmlHttp()
     xmlhttp.open('GET', globalPointsFilename, true);
     xmlhttp.onreadystatechange =
-	function() {
-	    if (xmlhttp.readyState == 4) {
-		if(xmlhttp.status == 200) {
-		    var points = parsePoints(xmlhttp.responseText);
-		    drawPath(points);
-		}
-	    }
-	};
+        function() {
+            if (xmlhttp.readyState == 4) {
+                if(xmlhttp.status == 200) {
+                    var points = parsePoints(xmlhttp.responseText);
+                    drawPath(points);
+                }
+            }
+        };
     xmlhttp.send(null);
 }
 
@@ -52,29 +52,29 @@ function parsePoints(text){
     var i = 0;
     var result = [];
     while(i < lines.length){
-	var tokens = lines[i].split(" ");
-	var time = parseInt(tokens[0]);
-	var lat = parseFloat(tokens[1]);
-	var lng = parseFloat(tokens[2]);
-	var acc = parseFloat(tokens[3]);
-	if(acc <= globalMaxAccuracy)
-	    result.push(new google.maps.LatLng(lat, lng));
-	i++;
+        var tokens = lines[i].split(" ");
+        var time = parseInt(tokens[0]);
+        var lat = parseFloat(tokens[1]);
+        var lng = parseFloat(tokens[2]);
+        var acc = parseFloat(tokens[3]);
+        if(acc <= globalMaxAccuracy)
+            result.push(new google.maps.LatLng(lat, lng));
+        i++;
     }
     return result;
 }
 
 function drawPath(points){
     if(globalPolyline != null)
-	globalPolyline.setMap(null);
+        globalPolyline.setMap(null);
     globalPolyline =
-	new google.maps.Polyline({
-	    path: points,
-	    geodesic: true,
-	    strokeColor: '#000080',
-	    strokeOpacity: 1.0,
-	    strokeWeight: 2
-	});
+        new google.maps.Polyline({
+            path: points,
+            geodesic: true,
+            strokeColor: '#000080',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
     globalPolyline.setMap(globalMap);
 }
 
@@ -87,22 +87,22 @@ function initialize() {
     var zoom = 0;
     var points = [];
     if(xmlhttp.status == 200)
-	points = parsePoints(xmlhttp.responseText);
+        points = parsePoints(xmlhttp.responseText);
     if(points.length > 0){
-	// center the map into a last checkpoint
-	center = points[points.length - 1];
-	zoom = 14;
+        // center the map into a last checkpoint
+        center = points[points.length - 1];
+        zoom = 14;
     }
     var mapOptions =
-	{
+        {
             center: center,
             zoom: zoom,
             mapTypeId: google.maps.MapTypeId.ROADMAP
-	};
+        };
     globalMap =
-	new google.maps.Map(
-	    document.getElementById("map_canvas"),
-	    mapOptions);
+        new google.maps.Map(
+            document.getElementById("map_canvas"),
+            mapOptions);
     drawPath(points);
 }
 
